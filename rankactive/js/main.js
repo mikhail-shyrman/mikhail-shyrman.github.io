7048bad39f17861d2e1ad29f2d8a197a2854855e
H4nteR <hanterm2@mail.ru>
2018-05-22 21:10:52 +0300

    added project

---
+++ rankactive/js/main.js
@@ -0,0 +1,12 @@
+$(function(){
+
+    $('select').customSelectMenu({
+        menuClass : 'my-menu',
+        openedClass : 'shown',
+        selectedClass : 'active',
+        selectionMadeClass : 'has-been-selected'
+    });
+
+});
+
+
